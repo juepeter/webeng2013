a0604e0d1b83409b8bd15a36190d79fb567330f4
Jürgen Peter <juergen.peter.berlin@gmx.de>
2013-10-16 22:38:58 +0200

    4. versuch index.php nur noch html-Inhalt

--- bootstrap/index.php
+++ bootstrap/index.php
@@ -1,4 +1,10 @@
-<?php
-	echo "Hello World";
-	phpinfo();
-?>
+<!DOCTYPE html>
+    <html>
+    <head>
+    <title>Bootstrap 101 Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+    <h1>Probleme der ersten Kurseinheit</h1>
+    </body>
+    </html>
